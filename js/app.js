67076ee23ad5b86220013cd8bd650aab180d882b
Antônio Murara Zabala <antoniomurar4@gmail.com>
2023-05-23 09:51:04 -0300

    audio colocado na aba da abertura de chamados

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// const cliente = document.getElementById('client').value
+// const sistema = document.getElementById('system').value
+// const modulo = document.getElementById('modulo').value
+// const manutencao_tipo = document.getElementById('manutencao-type').value
+// const tempo = document.getElementById('tdm').value
+// const titulo = document.getElementById('titulo').value
+// const descricao = document.getElementById('descricao').value
+// const res = document.getElementById('res')  
+// const enviar = document.getElementById('enviar')
+
+// enviar.addEventListener('click', (e)=>{
+//     e.preventDefault()
+//     res.innerHTML = `Seu cliente: ${cliente} quer uma manutenção ${manutencao_tipo} no sistema: ${sistema}, no módulo: ${modulo}, ele(a) quer que fica pronto dentro de ${tempo}.`
+//     console.log(cliente)
+// })
+
+const audio = document.querySelector(".audio");
+audio.play();
